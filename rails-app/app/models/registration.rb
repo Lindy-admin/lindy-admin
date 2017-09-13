539954778ce88ef89068afbac0beb07868ec2282
Theo van den Bogaart <todorus@todorus.com>
2017-09-13 19:46:48 +0200

    #12 added cascading when a member gets destroyed

--- rails-app/app/models/registration.rb
+++ rails-app/app/models/registration.rb
@@ -2,6 +2,6 @@ class Registration < ApplicationRecord
   belongs_to :member
   belongs_to :course
   belongs_to :ticket
-  has_one :payment
-  has_many :mailings
+  has_one :payment, dependent: :destroy
+  has_many :mailings, dependent: :destroy_all
 end
