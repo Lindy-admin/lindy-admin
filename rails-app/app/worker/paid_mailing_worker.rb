25371b6d43b28177242035cd13b634fbb161f8b4
Theo van den Bogaart <todorus@todorus.com>
2017-11-24 21:55:14 +0100

    #20 using settings for mailjet

--- rails-app/app/worker/paid_mailing_worker.rb
+++ rails-app/app/worker/paid_mailing_worker.rb
@@ -7,34 +7,44 @@ class PaidMailingWorker
     member = mailing.registration.member
     course = mailing.registration.course
 
-    sender_email = "todorus@todorus.com"
-    sender_name = "Theo is testing emails"
-    template_id = 210827
+    logger.info("Sending paid mail to member.id #{member.id}")
+
+    sender_email = Setting.mailjet_sender_email
+    sender_name = Setting.mailjet_sender_name
+    template_id = Setting.mailjet_payed_template_id.to_i
     variables = {
       member_firstname: member.firstname,
       member_lastname: member.lastname,
       course_title: course.title
     }
 
+    options = {
+      api_key: Setting.mailjet_public_api_key,
+      secret_key: Setting.mailjet_private_api_key
+    }
+
     response = Mailjet::Send.create(
-      messages: [
-        {
-          From: {
-            Email: sender_email,
-            Name: sender_name
-          },
-          To: [
-            {
-              Email: member.email,
-              Name: member.full_name
-            }
-          ],
-          TemplateID: template_id,
-          TemplateLanguage: true,
-          Subject: "Welcome to #{course.title}!",
-          Variables: variables
-        }
-      ]
+      {
+        messages: [
+          {
+            From: {
+              Email: sender_email,
+              Name: sender_name
+            },
+            To: [
+              {
+                Email: member.email,
+                Name: member.full_name
+              }
+            ],
+            TemplateID: template_id,
+            TemplateLanguage: true,
+            Subject: "Welcome to #{course.title}!",
+            Variables: variables
+          }
+        ]
+      },
+      options
     )
 
     message = response.Messages[0]
--- rails-app/config/initializers/mailjet.rb
+++ rails-app/config/initializers/mailjet.rb
@@ -1,5 +1,3 @@
 Mailjet.configure do |config|
-  config.api_key = ENV['MJ_APIKEY_PUBLIC']
-  config.secret_key = ENV['MJ_APIKEY_PRIVATE']
   config.api_version = "v3.1"
 end
