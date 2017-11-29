5a34eee792cd979e04688e73101b0bc4190c707f
Theo van den Bogaart <todorus@todorus.com>
2017-11-29 23:01:40 +0100

    added first registration tests

--- rails-app/spec/stories/registration_spec.rb
+++ rails-app/spec/stories/registration_spec.rb
@@ -9,6 +9,36 @@ describe "When registering for a course", type: :request do
 
   end
 
+  before(:each) do
+    stub_request(:post, "https://api.mollie.nl/v1/payments").
+    to_return(
+        status: 200,
+        headers:{
+          "Content-Type" => "application/json; charset=utf-8"
+        },
+        body: {
+          resource:        "payment",
+          id:              "tr_7UhSN1zuXS",
+          mode:            "test",
+          createdDatetime: "2017-11-29T12:22:59.0Z",
+          status:          "open",
+          expiryPeriod:    "PT15M",
+          amount:          10.00,
+          description:     "My first payment",
+          metadata: {
+              order_id: "12345"
+          },
+          locale: "nl",
+          profileId: "pfl_QkEhN94Ba",
+          links: {
+              paymentUrl:  "https://www.mollie.com/payscreen/select-method/7UhSN1zuXS",
+              redirectUrl: "https://webshop.example.org/order/12345/",
+              webhookUrl:  "https://webshop.example.org/payments/webhook/"
+          }
+      }.to_json
+    )
+  end
+
   context "while providing the correct input" do
 
     let (:params) do
@@ -28,7 +58,11 @@ describe "When registering for a course", type: :request do
       }.to change{Registration.count}.by(1)
     end
 
-    pending "will create a payment for the registration"
+    it "will create a payment for the registration" do
+      expect {
+        post registrations_path, params: params
+      }.to change{Payment.count}.by(1)
+    end
 
     pending "will send an email with payment instructions"
 
