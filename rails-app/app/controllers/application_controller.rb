de5e4cb94719684f7fc0e5e734b516c19d98a27b
Theo van den Bogaart <todorus@todorus.com>
2018-02-11 14:36:41 +0100

    #47 moved CSRF check in front, as this would conflict with Devise

--- rails-app/app/controllers/application_controller.rb
+++ rails-app/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
   before_action :authenticate_user!
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
