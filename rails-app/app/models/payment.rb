f3bceb06924fd7c0123d202f26d46c87df291660
Theo van den Bogaart <todorus@todorus.com>
2018-02-21 23:48:53 +0100

    #57mollie payments are created by a worker

--- rails-app/app/models/payment.rb
+++ rails-app/app/models/payment.rb
@@ -7,25 +7,19 @@ class Payment < ApplicationRecord
     created: 0,
     submitted: 1,
     paid: 2,
-    aborted: 3
+    aborted: 3,
+    failed: 4
   }
 
   after_create :submit_to_payment_provider
   after_save :send_confirmation_email, if: :status_changed?
 
   def submit_to_payment_provider
-    mollie = Mollie::API::Client.new(Setting.mollie_api_key)
-    mollie_payment = mollie.payments.create(
-      amount:       registration.ticket.price.fractional * 0.01,
-      description:  registration.course.title,
-      redirect_url: "#{Setting.mollie_redirect_url}?payment=#{self.id}",
-      webhook_url:  payment_webhook_url(Apartment::Tenant.current, self.id, host: Rails.application.config.webhook_hostname)
+    logger.info("qeueuing payment creation")
+    PaymentWorker.perform_async(
+      self.id,
+      payment_webhook_url(Apartment::Tenant.current, self.id, host: Rails.application.config.webhook_hostname)
     )
-
-    self.remote_id = mollie_payment.id
-    self.payment_url = mollie_payment.payment_url
-    self.status = :submitted
-    self.save!
   end
 
   def send_confirmation_email
