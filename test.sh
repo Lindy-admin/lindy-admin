a133820273965354443da573613e0678992db11d
Theo van den Bogaart <todorus@todorus.com>
2018-02-22 16:49:49 +0100

    #58 added testing script

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # docker build . -f deployment/app/Dockerfile -t todorus/lindy-admin:develop
-# docker-compose -f $LOCAL_DIR/deployment/test.yaml build
+docker-compose -f $LOCAL_DIR/deployment/test.yaml build
 docker-compose -f $LOCAL_DIR/deployment/test.yaml up --abort-on-container-exit --force-recreate
