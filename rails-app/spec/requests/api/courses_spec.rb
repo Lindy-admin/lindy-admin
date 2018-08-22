15d057dea0fae394ebdd378d5539dbf3f96ef9dc
Theo van den Bogaart <todorus@todorus.com>
2018-08-22 10:03:24 +0200

    #69 added test for filtering on api courses endpoint

--- rails-app/spec/requests/api/courses_spec.rb
+++ rails-app/spec/requests/api/courses_spec.rb
@@ -8,13 +8,38 @@ describe "When requesting the available courses" do
     @tenant = @user.tenant
   end
 
-  context "with a course that is open for registration" do
+  context "with courses that are open for registration" do
 
     before(:each) do
-      Apartment::Tenant.switch!(@tenant.token)
-      @course = FactoryBot.create(:course, registration_start: DateTime.now.to_date - 2.days, registration_end: DateTime.now.to_date + 2.days)
-      @ticket = FactoryBot.create(:ticket, course: @course)
-      Apartment::Tenant.reset
+      before(:each) do
+        Apartment::Tenant.switch!(@tenant.token)
+
+        @LOCATION_NIJMEGEN = "Nijmegen"
+        @LOCATION_DENBOSCH = "Den Bosch"
+
+        @STYLE_LINDY = "Lindy Hop"
+        @STYLE_SHAG = "Collegiate Shag"
+
+        @course1 = FactoryBot.create(:course, registration_start: DateTime.now.to_date - 2.days, registration_end: DateTime.now.to_date + 2.days)
+        @course1.location_list = [@LOCATION_NIJMEGEN]
+        @course1.style_list = [@STYLE_SHAG]
+        @course1.save!
+        @ticket1 = FactoryBot.create(:ticket, course: @course1)
+
+        @course2 = FactoryBot.create(:course, registration_start: DateTime.now.to_date - 2.days, registration_end: DateTime.now.to_date + 2.days)
+        @course2.location_list = [@LOCATION_NIJMEGEN, @LOCATION_DENBOSCH]
+        @course2.style_list = [@STYLE_LINDY]
+        @course2.save!
+        @ticket2 = FactoryBot.create(:ticket, course: @course2)
+        Apartment::Tenant.reset
+
+        @course3 = FactoryBot.create(:course, registration_start: DateTime.now.to_date - 2.days, registration_end: DateTime.now.to_date + 2.days)
+        @ticket3 = FactoryBot.create(:ticket, course: @course3)
+        @course3.location_list = [@LOCATION_DENBOSCH]
+        @course3.style_list = [@STYLE_LINDY, @STYLE_SHAG]
+        @course3.save!
+        Apartment::Tenant.reset
+      end
     end
 
     let (:headers) do
@@ -23,15 +48,68 @@ describe "When requesting the available courses" do
       }
     end
 
-    it "will return the course" do
-      get api_courses_path @tenant.token, headers: headers
+    context "without a filter"
 
-      expect(response.status).to be(200)
+      let get_courses {
+        get api_courses_path @tenant.token, headers: headers
+      }
 
-      body = JSON.parse response.body
-      expect(body.length).to eq(1)
-      expect(body[0]["id"]).to eq(@course.id)
-      expect(body[0]["title"]).to eq(@course.title)
+      it "will return all the courses" do
+        get_courses
+        expect(response.status).to be(200)
+
+        body = JSON.parse response.body
+        expect(body.length).to eq(3)
+        expect(body[0]["id"]).to eq(@course1.id)
+        expect(body[0]["title"]).to eq(@course1.title)
+        expect(body[1]["id"]).to eq(@course2.id)
+        expect(body[1]["title"]).to eq(@course2.title)
+        expect(body[2]["id"]).to eq(@course3.id)
+        expect(body[2]["title"]).to eq(@course3.title)
+      end
+
+    end
+
+    context "with a location filter"
+
+      let get_courses {
+        get api_courses_path @tenant.token, headers: headers, params: {location: @LOCATION_NIJMEGEN}
+      }
+
+      it "will return the courses only for that location" do
+        get_courses
+        expect(response.status).to be(200)
+
+        body = JSON.parse response.body
+        expect(body.length).to eq(2)
+        expect(body[0]["id"]).to eq(@course1.id)
+        expect(body[0]["title"]).to eq(@course1.title)
+
+        expect(body[1]["id"]).to eq(@course2.id)
+        expect(body[1]["title"]).to eq(@course2.title)
+      end
+
+    end
+
+    context "with a style filter"
+
+      let get_courses {
+        get api_courses_path @tenant.token, headers: headers, params: {style: @STYLE_SHAG}
+      }
+
+      it "will return the courses only for that location" do
+        get_courses
+        expect(response.status).to be(200)
+
+        body = JSON.parse response.body
+        expect(body.length).to eq(2)
+        expect(body[0]["id"]).to eq(@course1.id)
+        expect(body[0]["title"]).to eq(@course1.title)
+
+        expect(body[1]["id"]).to eq(@course3.id)
+        expect(body[1]["title"]).to eq(@course3.title)
+      end
+
     end
 
   end
